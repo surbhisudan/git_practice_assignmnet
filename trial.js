2d770fd0e4735b3b2f21a9ae78f530eb108318f8
Surbhi Sudan <surbhi.sudan02@gmail.com>
2023-01-06 21:11:09 +0530

    palindrome and prime problem done

---
+++ trial.js
@@ -0,0 +1,6 @@
+let str="masai";
+let bag=""
+for(i=str.length;i>=0;i--){
+  bag+=str[i]
+  
+}console.log(bag)
---
+++ trial2.js
@@ -0,0 +1,12 @@
+let a=5;
+let c=0;
+for(i=0;i<a;i++){
+  if(a%i==0){
+    c++
+  }
+}if(c==2){
+  console.log("true");
+}
+else{
+  console.log("false")
+}
